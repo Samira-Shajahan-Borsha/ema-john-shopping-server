15a587d6b79963b413ba8019563042dc0dacbeaf
Samira Shajahan Borsha <57401425+Samira-Shajahan-Borsha@users.noreply.github.com>
2023-04-01 01:45:06 +0600

    created an api to get all products

--- index.js
+++ index.js
@@ -10,12 +10,29 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
 //database connected
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.d0roctp.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const productCollection = client.db('emaJohn').collection('products');
+
+        //get all products
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.limit(10).toArray();
+            res.send(products);
+        });
+    }
+    finally {
+
+    }
+}
+run().catch(error => console.log(error))
+
+
 
 
 
